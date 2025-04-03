af3fcfb5da2fac5457623b02807cef2703fd99dd
DannyCollinson <dannycollinson12@gmail.com>
2025-04-02 23:23:22 -0400

    Fixed __init__.py and updated README

--- src/torch_focalloss/__init__.py
+++ src/torch_focalloss/__init__.py
@@ -1,4 +1,6 @@
 """Set up torch_focalloss package"""
 
 # make the classes available directly from the top level
-from .losses import BinaryFocalLoss, MultiClassFocalLoss  # type: ignore
+from torch_focalloss.losses import BinaryFocalLoss, MultiClassFocalLoss
+
+__all__ = ["BinaryFocalLoss", "MultiClassFocalLoss"]
